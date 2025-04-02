65e1717f9cdf92c3f5f6f35f285707eec8a62b4d
rashmi3112 <rashmiunhale12@gmail.com>
2025-04-02 11:59:03 +0530

    Changes in Program and appsettings for backend deployment

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,21 @@ using AspNetCoreRateLimit;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Load email credentials securely
+var smtpServer = Environment.GetEnvironmentVariable("SMTP_SERVER") ?? "smtp.gmail.com";
+var smtpPort = Environment.GetEnvironmentVariable("SMTP_PORT") ?? "587";
+var smtpEmail = Environment.GetEnvironmentVariable("SMTP_EMAIL") ?? "rashmiunhale086@gmail.com";
+var smtpPassword = Environment.GetEnvironmentVariable("SMTP_PASSWORD") ?? "dpyz ujfi pubo avtu";
+
+//Kestral configuration for Render
+builder.WebHost.ConfigureKestrel(serverOptions => {
+    serverOptions.ListenAnyIP(5000);
+});
+
+//logging for Render debugging
+var logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger("App");
+logger.LogInformation("API is starting...");
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -11,7 +26,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
-        policy => policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
+        policy => policy.WithOrigins("http://localhost:4200", "https://localhost:4200", "https://rashmi3112.github.io")
             .AllowAnyMethod()
             .AllowAnyHeader());
 });
@@ -49,5 +64,8 @@ app.UseCors("AllowFrontend");
 app.UseAuthorization();
 app.UseIpRateLimiting();
 app.MapControllers();
+
+//logger
+logger.LogInformation("API is running");
 
 app.Run();
