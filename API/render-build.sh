aebbd36d6bc1e78a99e5bd8b4d07a1b8c2f9a7a9
rashmi3112 <rashmiunhale12@gmail.com>
2025-04-02 19:11:52 +0530

    render-build.sh for correct .NET deployment

--- API/render-build.sh
+++ API/render-build.sh
@@ -6,7 +6,5 @@ export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$DOTNET_ROOT:$PATH
 
 dotnet --info  # Check if .NET is installed
-dotnet restore
-
-#Build a self-contained executable for Linux
-dotnet publish src/API/API.csproj -c Release -r linux-x64 --self-contained true -o src/API/out
+dotnet restore API/API.csproj
+dotnet publish API/API.csproj -c Release -r linux-x64 --self-contained true -o API/out
