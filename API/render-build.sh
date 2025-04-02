32a5296a03e69a2e15090f441ef5fb90d1f58a53
rashmi3112 <rashmiunhale12@gmail.com>
2025-04-02 19:56:26 +0530

    Permission render-build.sh for correct .NET deployment

--- API/render-build.sh
+++ API/render-build.sh
@@ -8,3 +8,6 @@ export PATH=$DOTNET_ROOT:$PATH
 dotnet --info  # Check if .NET is installed
 dotnet restore API/API.csproj
 dotnet publish API/API.csproj -c Release -r linux-x64 --self-contained false -o API/out
+
+# Ensure the built file has execute permissions
+chmod +x API/out/API.dll
